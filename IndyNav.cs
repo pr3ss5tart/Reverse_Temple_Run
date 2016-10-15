a44720a3f18cc0b4f6213acabd38a5f01ec1b384
mokk55 <threepointonefourfive@gmail.com>
2016-10-14 19:14:16 -0700

    Add files via upload
    Nav mesh

---
+++ IndyNav.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndyNav : MonoBehaviour {
+
+	public GameObject IndyTarget;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<NavMeshAgent> ().SetDestination (IndyTarget.transform.position);
+	}
+}
